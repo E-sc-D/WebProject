f1a87734638005d8b342d2cb69d6567d2043bc0d
adisdema <137163780+adisdema@users.noreply.github.com>
2026-01-19 20:33:59 +0100

    modifiche side bar
    chiudo sidebar su schermo picolo se clichi su un spazio vuoto

--- templates/script.js
+++ templates/script.js
@@ -1,9 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
     const sidebarToggle = document.getElementById('sidebarToggle');
+    // Toggle con bottone
     if (sidebarToggle) {
         sidebarToggle.addEventListener('click', (e) => {
             e.preventDefault();
+            e.stopPropagation(); // evita chiusura immediata
             document.body.classList.toggle('sb-sidenav-toggled');
         });
     }
-});
+    // Click fuori dalla sidebar → chiudi
+    document.addEventListener('click', (e) => {
+        
+        if (window.innerWidth < 992) {
+            document.body.classList.remove('sb-sidenav-toggled');
+        }
+    });
+})
